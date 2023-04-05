0a75f6e8ac108d60ee5f3d9a7de7fc8b2af27dd0
mnxk2001 <mngxuankieu1905@gmail.com>
2023-04-05 22:48:44 +0700

    register is done

--- branchKieu/QuanLy/index.py
+++ branchKieu/QuanLy/index.py
@@ -20,40 +20,33 @@ def login():
 def arlets():
     return render_template('arlets.html')
 
-# @app.route("/admin/register")
-# def register():
-#
-#     return render_template('register.html', getRole=getRole)
-
-@app.route("/admin/register", methods=["GET", "POST"])
+@app.route("/admin/register")
 def register():
+    getRole = dao.get_roles()
+    return render_template('register.html', getRole=getRole)
+
+@app.route("/admin/register", methods=["POST"])
+def register_process():
 
     getRole = dao.get_roles()
-    error = None
 
-    if request.method == 'post':
+    if request.form['username'] != Users.query.filter(Users.username == request.form['username']):
+
+        name = request.form["name"]
+        username = request.form["username"]
+        pwd = request.form["password"]
+        phone = request.form["phone"]
+        role_id = int(request.form["idRole"])
+
         try:
-            if request.form['username'] != Users.query.filter(Users.username==request.form['username']):
-                name = request.form["name"]
-                username = request.form["username"]
-                pwd = request.form["password"]
-                phone = request.form["phone"]
-                role_id = int(request.form["idRole"])
-
-                dao.add_users(name=name, username=username, password=pwd, phone=phone, role_id=role_id)
-                return redirect(url_for('admin'))
-
-            else:
-                error = 'Tài khoản đăng nhập đã tồn tại'
-
+            dao.add_users(name=name, username=username, password=pwd, phone=phone, role_id=role_id)
+            msg = 'Tạo thành công'
         except Exception as ex:
-            error = str(ex)
+            msg = 'Tài khoản đăng nhập đã tồn tại' + str(ex)
+    else:
+        msg = 'Tạo thành công'
 
-    return render_template('register.html', error=error, getRole=getRole)
-
-
-
-
+    return render_template('register.html', msg=msg, getRole=getRole)
 
 if __name__ == '__main__':
     app.run(debug=True)
