13e4bb78e7129443f71943e2722eff3e2fc6fb66
mnxk2001 <mngxuankieu1905@gmail.com>
2023-04-08 17:39:34 +0700

    Gio hang thanh toan them vao gio

--- QuanLy/__init__.py
+++ QuanLy/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote
 from flask_login import LoginManager
+import cloudinary
 
 app = Flask(__name__)
 app.secret_key = '249ru3trrgt4567%#32l83beligut*^^%$'
@@ -11,3 +12,5 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 login = LoginManager(app)
+
+cloudinary.config(cloud_name='dkyr4znvv', api_key='212769388125549', api_secret='K4OF9_dgoYn-w1FBPibqnhBPPqI')
